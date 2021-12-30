144af822d0e0715ba74a8c112a597829c16a05de
minhyeong <rudolh@knou.ac.kr>
2021-12-30 15:43:53 +0900

    socketio 셋업

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,64 +1,72 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
+const socket = io();
 
-// const socket = new WebSocket("ws://localhost:3000");
-const socket = new WebSocket(`ws://${window.location.host}`);
-// 여기서의 소켓은 서버로의 연결
+// ws용 코드
+// const messageList = document.querySelector("ul");
+// const messageForm = document.querySelector("#message");
+// const nickForm = document.querySelector("#nick");
 
-function makeMessage(type, payload) {
-    const msg = {type, payload};
-    return JSON.stringify(msg);
-}
-// 백엔드는 스트링으로 받는다
-// 왜냐면 frontend는 여러 언어일 수 있기도 하고...
-// 백엔드에서는 다시 오브젝트로 바꿔줘야겠지
+// // const socket = new WebSocket("ws://localhost:3000");
+// const socket = new WebSocket(`ws://${window.location.host}`); // 이것은 socketIo와 호환되진 않음... 그래서 socketio url을 통해 클라이언트가 임포트함.
+// // 여기서의 소켓은 서버로의 연결
 
-function handleOpen() {
-    console.log("Connected to Server V");
-}
+// function makeMessage(type, payload) {
+//     const msg = {type, payload};
+//     return JSON.stringify(msg);
+// }
+// // 백엔드는 스트링으로 받는다
+// // 왜냐면 frontend는 여러 언어일 수 있기도 하고...
+// // 백엔드에서는 다시 오브젝트로 바꿔줘야겠지
 
-socket.addEventListener("open", handleOpen);
-
-// socket.addEventListener("open", () => {
+// function handleOpen() {
 //     console.log("Connected to Server V");
+// }
+
+// socket.addEventListener("open", handleOpen);
+
+// // socket.addEventListener("open", () => {
+// //     console.log("Connected to Server V");
+// // });
+
+// socket.addEventListener("message", (message) => {
+//     // console.log("New message: ", message);
+//     const li = document.createElement("li");
+//     li.innerText = message.data;
+//     messageList.append(li);
 // });
 
-socket.addEventListener("message", (message) => {
-    // console.log("New message: ", message);
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
+// socket.addEventListener("close", () => {
+//     console.log("Connected from Server X");
+// });
 
-socket.addEventListener("close", () => {
-    console.log("Connected from Server X");
-});
+// // setTimeout(() => {
+// //     socket.send("Hello from the browser!");
+// // }, 10000);
 
-// setTimeout(() => {
-//     socket.send("Hello from the browser!");
-// }, 10000);
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const input = messageForm.querySelector("input");
+//     // console.log(input.value);
+//     socket.send(makeMessage("new_message", input.value));
+//     const li = document.createElement("li");
+//     li.innerText = message.data;
+//     // li.innerText = `You: ${message.data}`;
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    // console.log(input.value);
-    socket.send(makeMessage("new_message", input.value));
-    input.value = "";
-}
+//     messageList.append(li);
+//     input.value = "";
+// }
 
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    // socket.send(input.value); // 이러면 메시지로 보내지고... 메시지 종류 분할 필요
-    // JSON으로 보내자
-    // socket.send({
-    //     type:"nickname",
-    //     payload:input.value,
-    // });
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-}
+// function handleNickSubmit(event) {
+//     event.preventDefault();
+//     const input = nickForm.querySelector("input");
+//     // socket.send(input.value); // 이러면 메시지로 보내지고... 메시지 종류 분할 필요
+//     // JSON으로 보내자
+//     // socket.send({
+//     //     type:"nickname",
+//     //     payload:input.value,
+//     // });
+//     socket.send(makeMessage("nickname", input.value));
+//     input.value = "";
+// }
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+// messageForm.addEventListener("submit", handleSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -14,11 +15,18 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on https://localhost:3000`)
 // app.listen(3000, handleListen);
 
-// express 는 https protocol을 다루지만 ws는 wss Web Socket 프로토콜을 다룬다
+// express 는 https protocol을 다루지만 ws는 wsServer Web Socket 프로토콜을 다룬다
 // 합쳐보자
 
-const server = http.createServer(app); //http 서버
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app); //http 서버
+// const wsServer = new WebSocket.Server({ httpServer });
+const wsServer = SocketIO(httpServer);
+// localhost:3000/socket.io/socket.io.js
+
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+});
+
 // server를 굳이넣지 않아도 되지만
 // 안에 server를 전달함으로써 http 서버와 WebSocket을 같이 돌릴 수 있다
 // 같은 서버 같은 포트에서 http websocket을 둘 다 작동시키기 위함
@@ -28,8 +36,8 @@ const wss = new WebSocket.Server({ server });
 //     console.log(socket);
 // }
 // 여기서의 소켓은 브라우저임
-// wss.on("connection", handleConnection);
-// wss.on("connection", (socket) => {
+// wsServer.on("connection", handleConnection);
+// wsServer.on("connection", (socket) => {
 //     console.log(socket);
 // })
 
@@ -41,39 +49,40 @@ function onSocketClose() {
 //     console.log(message.toString('utf8'));
 // }
 
-const sockets = [];
-// fake database
-// 누군가 서버에 연결하면 connection을 sockets에 넣음
+// Web Socket 기반
+// const sockets = [];
+// // fake database
+// // 누군가 서버에 연결하면 connection을 sockets에 넣음
 
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser V");
-    socket.on("close", onSocketClose);
-    // socket.on("message", onSocketMessage);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type){
-            case "new_message":
-                // sockets.forEach(aSocket => aSocket.send(message.payload.toString('utf8')));
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`)
-                );
-                break;
-            case "nickname":
-                // console.log(message.payload);
-                socket["nickname"] = message.payload;
-                break;
-        }
-        // if (message.type === "new_message") {
-        //     sockets.forEach(aSocket => aSocket.send(message.payload.toString('utf8')));
-        // } else if (message.type === "nickname") {
+// wsServer.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser V");
+//     socket.on("close", onSocketClose);
+//     // socket.on("message", onSocketMessage);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type){
+//             case "new_message":
+//                 // sockets.forEach(aSocket => aSocket.send(message.payload.toString('utf8')));
+//                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`)
+//                 );
+//                 break;
+//             case "nickname":
+//                 // console.log(message.payload);
+//                 socket["nickname"] = message.payload;
+//                 break;
+//         }
+//         // if (message.type === "new_message") {
+//         //     sockets.forEach(aSocket => aSocket.send(message.payload.toString('utf8')));
+//         // } else if (message.type === "nickname") {
             
-        // }
-    });
-    // socket.send("Hello!");
-});
+//         // }
+//     });
+//     // socket.send("Hello!");
+// });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
 // app 에서 별로 안바뀐것 같지만 http서버 위에 websocket을 같이...
 
 
